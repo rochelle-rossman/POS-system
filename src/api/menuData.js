9cb7990b489cc9b975ea7a122f5b198f7f6b1071
Phantom-Farmer <joeljdahl@gmail.com>
2022-06-15 22:21:02 -0500

    basic api calls for menu items, but with some of the parameters and values blank

--- src/api/menuData.js
+++ src/api/menuData.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const dbUrl = 'https://hip-hop-pizza-and-wangs-default-rtdb.firebaseio.com'
 
-const getMenuItems = (uid) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/menus.json?orderBy="uid"&equalTo="${uid}"`)
+const getMenuItems = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/menus.json?orderBy="uid"&equalTo="${}"`)
     .then((response) => {
       if (response.data) {
         resolve(Object.values(response.data));
@@ -13,13 +13,34 @@ const getMenuItems = (uid) => new Promise((resolve, reject) => {
     }).catch((error) => reject(error));
 });
 
-const createNewMenuItem = (obj, uid) => new Promise((resolve, reject) => {
+const createNewMenuItem = () => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/menus.json`, obj)
     .then((response) => {
-      const giveFBK = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/menus/${response.data.name}.json`, giveFBK)
+      const addFBK = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/menus/${response.data.name}.json`, addFBK)
         .then(() => {
-          getMenuItems(uid).then((wordsArray) => resolve(wordsArray));
+          getMenuItems().then((menusArray) => resolve(menusArray));
         });
     }).catch((error) => reject(error));
 });
+
+const updateMenuItem = (obj, firebaseKey) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/menus/${obj.firebaseKey || firebaseKey}.json`, obj)
+    .then(() => {
+      getMenuItems(obj.).then((menusArray) => resolve(menusArray));
+    }).catch((error) => reject(error));
+});
+
+const deleteMenuItem = (firebaseKey, ) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/menus/${firebaseKey}.json`)
+    .then(() => {
+      getMyWords().then((menusArray) => resolve(menusArray));
+    }).catch((error) => reject(error));
+});
+
+export {
+  getMenuItems, 
+  createNewMenuItem, 
+  updateMenuItem, 
+  deleteMenuItem
+}
