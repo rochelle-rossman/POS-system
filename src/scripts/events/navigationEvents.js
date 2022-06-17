d32c8e1979224c60691bc091b2e6ff17307edf87
Keaton Law <keaton.c.law@gmail.com>
2022-06-16 19:28:09 -0500

    merging

--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -1,8 +1,12 @@
 import signOut from '../helpers/auth/signOut';
+import renderRevenue
 
 const navEvt = () => {
   document.querySelector('#logout')
     .addEventListener('click', signOut);
+  document.querySelector('#revLink').addEventListener('click', () => {
+    renderRevenue();
+  });
   // document.querySelector('#view-orders').addEventListener('click', () => {
   //  getOrders().then((ordersArray) => showOrders(ordersArray));
   // })
