317e4083e21b7e20cb68edcd68dd043e3ea48def
Michael Pennington <mwpennington@live.com>
2022-06-16 19:17:10 -0500

    finished with rendering the revenue

--- src/scripts/events/revEvent.js
+++ src/scripts/events/revEvent.js
@@ -1,9 +1,8 @@
 import renderRevenue from '../components/showRevenue';
-import { getRevenue } from '../../api/revenueData';
 
 const revEvent = () => {
   document.querySelector('#revLink').addEventListener('click', () => {
-    getRevenue().then((revenueObj) => renderRevenue(revenueObj));
+    renderRevenue();
   });
 };
 
