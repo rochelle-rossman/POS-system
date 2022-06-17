5103722cf1187aea7b176938c239ef1f21fe3c80
Michael Pennington <mwpennington@live.com>
2022-06-16 21:01:20 -0500

    ready to pull

--- src/scripts/components/orderCards.js
+++ src/scripts/components/orderCards.js
@@ -5,7 +5,7 @@ const noOrders = () => {
   document.querySelector('#main-container').innerHTML = '<h1>No Orders</h1>';
 };
 
-const showOrders = (array) => {
+const viewOrders = (array) => {
   clearDom();
 
   if (array.length) {
--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -1,11 +1,15 @@
 import signOut from '../helpers/auth/signOut';
 import renderRevenue from '../components/showRevenue';
-
+import viewOrders from '../components/orderCards';
+// Render Orders using view Orders
 const navEvt = () => {
   document.querySelector('#logout')
     .addEventListener('click', signOut);
   document.querySelector('#revLink').addEventListener('click', () => {
     renderRevenue();
+  });
+  document.querySelector('#logo').addEventListener('click', () => {
+    viewOrders();
   });
 };
 
