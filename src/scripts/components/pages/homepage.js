1cb07fe9bd1f49450663823061f651f0fb2eea6c
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-06-20 18:27:23 -0500

    interpolated user.displayName on homepage greeting

--- src/scripts/components/pages/homepage.js
+++ src/scripts/components/pages/homepage.js
@@ -1,10 +1,10 @@
 import clearDom from '../../helpers/clearDom';
 import renderToDOM from '../../helpers/renderToDom';
 
-const homePage = () => {
+const homePage = (user) => {
   clearDom();
   const domString = `
-    <h1>Welcome, Person that is using this app</h1>
+    <h1>Welcome, ${user.displayName}! </h1>
     <div class="group">
     <button type="button" class="btn btn-success btn-lg">View Orders</button>
     <button type="button" class="btn btn-info btn-lg">Create an Order</button>
--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -3,22 +3,17 @@ import renderNav from '../components/navBar';
 import navEvt from '../events/navigationEvents';
 import domEvents from '../events/domEvents';
 import homePage from '../components/pages/homepage';
-// import { showCustomers } from '../components/pages/customers';
-// import { showCustomers } from '../components/pages/customers';
 import fromEvt from '../events/formEvents';
 import btnEvt from '../events/buttonEvents';
 
-const startApp = () => {
+const startApp = (user) => {
   domBuilder();
   renderNav();
   navEvt();
   domEvents();
-  homePage();
-  // getCustomers().then((customerArray) => showCustomers(customerArray));
+  homePage(user);
   fromEvt();
   btnEvt();
-
-//  getCustomers().then((customerArray) => showCustomers(customerArray));
 };
 
 export default startApp;
